3f713e9a774b8ef04e5a4431bd3e4e49443c6587
Nima-sltn <nimaso8228@gmail.com>
2025-07-25 01:29:55 +0330

    feat: add .env and .env.example

--- src/app/blogs/_components/CategoryList.tsx
+++ src/app/blogs/_components/CategoryList.tsx
@@ -12,7 +12,7 @@ export interface Category {
 }
 
 const CategoryList = async () => {
-  const res = await fetch("http://localhost:5000/api/category/list");
+  const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/category/list`);
   const {
     data: { categories },
   } = await res.json();
