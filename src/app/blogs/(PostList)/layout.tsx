44cd9ea2bd2e245e9c736cf2b69fb43fc7e7c311
Nima-sltn <nimaso8228@gmail.com>
2025-08-14 18:01:48 +0330

    feat(Search): add search box component

--- src/app/blogs/(PostList)/layout.tsx
+++ src/app/blogs/(PostList)/layout.tsx
@@ -2,6 +2,7 @@ import { Metadata } from "next";
 import CategoryList from "../_components/CategoryList";
 import { Suspense } from "react";
 import Spinner from "@/ui/Spinner/Spinner";
+import Search from "@/ui/Search/Search";
 
 export const metadata: Metadata = {
   title: "بلاگ ها",
@@ -13,8 +14,11 @@ const layout = ({
   children: React.ReactNode;
 }>) => {
   return (
-    <div>
-      <h1 className="mb-12 text-lg font-bold">لیست بلاگ ها</h1>
+    <>
+      <div className="mb-12 grid grid-cols-1 items-center gap-8 text-secondary-700 lg:grid-cols-3">
+        <h1 className="mb-12 text-lg font-bold">لیست بلاگ ها</h1>
+        <Search />
+      </div>
       <div className="grid grid-cols-12 gap-8">
         <div className="col-span-12 space-y-4 text-secondary-500 lg:col-span-4 xl:col-span-3">
           <Suspense fallback={<Spinner />}>
@@ -25,7 +29,7 @@ const layout = ({
           {children}
         </div>
       </div>
-    </div>
+    </>
   );
 };
 
